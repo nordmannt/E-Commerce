187fd4d77f194fd753a6928aadd1c558aa7c9ba8
Tyson Nordamnn <tyson.nordmann@g.austincc.edu>
2025-04-07 14:28:10 -0500

    and more setup with some stubs

--- ecommerce-backend/src/main/java/com/revature/ecommerce_backend/controllers/UserController.java
+++ ecommerce-backend/src/main/java/com/revature/ecommerce_backend/controllers/UserController.java
@@ -1,9 +1,25 @@
 package com.revature.ecommerce_backend.controllers;
+
 import org.springframework.stereotype.Controller;
+import com.revature.ecommerce_backend.models.User;
+import com.revature.ecommerce_backend.services.UserService;
+
 
 @Controller
 public class UserController {
     
-   
+   private final UserService userService;
+
+   public UserController(UserService userService){
+    this.userService = userService;
+    }
+
+    public User createUser(User user) {
+        return this.userService.save(user);
+    }
+
+    public User getUser(Integer id) {
+        return this.userService.findById(id);
+    }
     
 }
